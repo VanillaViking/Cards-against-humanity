6c235611ed6646bd0ac398c65b317290feced00a
ashwinr2k2@gmail.com <ashwinr2k2@gmail.com>
2019-04-20 22:36:44 +1000

    GUI version now assigns cards to players

--- C.A.H/guimain.py
+++ C.A.H/guimain.py
@@ -1,12 +1,35 @@
 import pygame
 from starting_screen import *
 from settings_screen import *
+from players import *
+from cards import *
 pygame.init()
 DISPLAY = pygame.display.set_mode((1920, 1080))
 
-SCREENS = []
-SCREENS.append(starting_screen())
-SCREENS.append(settings_screen())
+#SCREENS
+starting = starting_screen()
+settings = settings_screen()
 
-for n in SCREENS:
-    n.draw(DISPLAY)
+
+#GAME START
+starting.draw(DISPLAY)
+settings.draw(DISPLAY)
+
+#creating players...
+PlayerNames = []
+temp = settings.player_names.split(",")
+for n in temp:
+    PlayerNames.append(n.strip())
+
+PLAYERS = [PLAYER(n) for n in PlayerNames]
+
+#creating bot players...
+for n in range(settings.number_of_bots):
+    name = "BOT" + str(n)
+    PLAYERS.append(PLAYER(name))
+
+#assign each player 7 cards...
+for n in range(len(PLAYERS)):
+    for s in range(7):
+        PLAYERS[n].assign_cards(WHITE.pop(random.randint(0,(len(WHITE)-1))))     #Gives each player four random cards
+
--- C.A.H/main.py
+++ C.A.H/main.py
@@ -2,9 +2,10 @@ from cards import *
 from players import *
 import random
 import os
+import time
 player_picks = []
 won = False
-clear = lambda: os.system('cls')
+clear = lambda: os.system('clear')
 
 
 playerNames = []
@@ -20,6 +21,16 @@ while True:
                 break
         except ValueError:
                 input("Enter a number, buddy")
+        
+while True:                                     #if want bot player
+        clear()
+        how_many_bot = int(input("how many bots? "))
+        for n in range(how_many_bot):
+                name = "BOT" + str(n)
+                PLAYERS.append(PLAYER(name))
+        break
+
+clear()
 
 #RAndomly giving each player four cards. Also this could probably go in players.py, something to think about later
 for n in range(len(PLAYERS)):
@@ -31,15 +42,18 @@ x = 0
 
 
 while not won:
+        if x >= len(PLAYERS):
+                x = 0
+                
         black_card = BLACK[random.randint(0,len(BLACK)-1)]
 
         PLAYERS[x].toggle_card_czar()
-        print("\n----------------\n\n\n%s is the card czar\n\n\n----------------\n" % PLAYERS[x].name)
+        print( "\n----------------\n\n\n%s is the card czar\n\n\n----------------\n" % PLAYERS[x].name)
         input("Enter to continue")
         clear()
 
         for l in range(len(PLAYERS)):
-                if not PLAYERS[l].is_card_czar:
+                if not PLAYERS[l].is_card_czar and "BOT" not in PLAYERS[l].name:
                         clear()
                         input("%s's turn \n\n Enter to continue\n" % PLAYERS[l].name)
                         while True:
@@ -56,6 +70,21 @@ while not won:
                                 except IndexError:
                                         clear()
                                         input("You don't have a card corresponding with that number.")
+                
+                if "BOT" in PLAYERS[l].name:            #BOT's choice
+                        bot_choice = random.randint(0, len(PLAYERS[l].cards)-1)
+                        PLAYERS[l].chosen_card += PLAYERS[l].cards.pop(bot_choice)
+
+        for nn in range(len(PLAYERS)):
+                if not PLAYERS[nn].is_card_czar:
+                        player_picks.append(PLAYERS[nn].chosen_card)
+
+        temp =[]
+        for pick in player_picks:
+                temp.append(player_picks.pop(random.randint(0,len(player_picks)-1)))
+        for temp_pick in temp:
+                player_picks.append(temp_pick)
+
 
         for v in range(len(PLAYERS)):
                 if PLAYERS[v].is_card_czar:
@@ -65,16 +94,34 @@ while not won:
                                 
                         while True:
                                 print(black_card, "\n\n")
-                                for i in range(len(PLAYERS)):      #prints each person's chosen card for the judge to choose
-                                        if not PLAYERS[i].is_card_czar:
-                                                print(PLAYERS[i].chosen_card, "\n")
-                                                player_picks.append(PLAYERS[i].chosen_card)
-                                try:        
-                                        winner_card = int(input("choose a card: "))
+                                for n in player_picks:             #print player picks for judging
+                                        print(n)
+                                try:   
+                                        if "BOT" not in PLAYERS[v].name:     
+                                                winner_card = int(input("choose a card: "))
+                                                clear()
+                                        else:
+                                                input("Enter to continue")
+                                                winner_card = random.randint(1,len(player_picks))                                                
+                                                for n in range(0,3): 
+                                                        clear()    
+                                                        print("BOT is choosing.")
+                                                        time.sleep(0.4)
+                                                        clear()
+                                                        print("BOT is choosing..")
+                                                        time.sleep(0.4)
+                                                        clear()
+                                                        print("BOT is choosing...")
+                                                        time.sleep(0.4)
+                                                        clear()
+                                                        print("BOT is choosing....")
+                                                        time.sleep(0.4)
+                                                        clear()
+                                                print("BOT chose:  %s\n" % player_picks[winner_card-1])
+
                                         for u in range(len(PLAYERS)):
-                                                if not PLAYERS[i].is_card_czar:
+                                                if not PLAYERS[u].is_card_czar:
                                                         if player_picks[winner_card-1] == PLAYERS[u].chosen_card:     #identifies who the chosen card belongs to
-                                                                clear()
                                                                 print("%s got a point!" % PLAYERS[u].name)
                                                                 PLAYERS[u].give_point()
                                                                 input("Enter to continue:")
@@ -100,10 +147,6 @@ while not won:
 
         PLAYERS[x].toggle_card_czar()   #toggle card czar since their turn is done
         x += 1
-        if x >= len(PLAYERS):
-                x = 0
 
-        
 
 print("Looks like %s went SICKO MODE!" % winner)
-input("Enter to continue")
